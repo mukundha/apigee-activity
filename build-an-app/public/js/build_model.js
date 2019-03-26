e43d647632850b66b6a1c9d24c5ef3de089986fb
Mukundha Madhavan Gnana Sekaran <gnanasekaran@gnanasekaran-macbookpro.roam.corp.google.com>
2019-03-26 08:29:15 +0000

    moving to one app

--- build-an-app/public/js/build_model.js
+++ build-an-app/public/js/build_model.js
@@ -1,6 +1,6 @@
-var model ={
+var build_model ={
     "start": {
-        "image": "images/1.png",
+        "image": "images/build_app/1.png",
         "text" : "Are you ready?",
         "actions" : [
             {
@@ -10,7 +10,7 @@ var model ={
         ]
     },
     "challenge-accepted" : {
-        "image": "images/11.png",
+        "image": "images/build_app/11.png",
         "text" : "How do you approach?",
         "actions" : [
             {
@@ -24,7 +24,7 @@ var model ={
         ]
     },
     "start-with-apibackend" : {
-        "image" : "images/2.png",
+        "image" : "images/build_app/2.png",
         "text" : "Web, Mobile, Voice are different channels on how your users will experience your store functionality, features. Best practice is to build your functionality/features as a independent service (usually referred to a backend application) with well defined remote interefaces (APIs), Good Choice, what's Next?",
         "actions": [
             {
@@ -38,7 +38,7 @@ var model ={
         ]
     },
     "lets-get-coding": {
-        "image": "images/blank.png",
+        "image": "images/build_app/blank.png",
         "text": "While it's tempting to get coding, remember other applications (web, mobile, voice) are dependent on your work, and they are usually developed by different teams (there's a hint here), so its usually a best practice to start with an OpenAPI specification to describe the application's interfaces and make sure this will help deliver the user experiences you (your business) wants to deliver",
         "actions": [
             {
@@ -49,7 +49,7 @@ var model ={
 
     },
     "are-you-sure-about-webapp": {
-        "image": "images/blank.png",
+        "image": "images/build_app/blank.png",
         "text" : "Web, Mobile, Voice are different channels on how your users will experience your store functionality, features. Best practice is to build your functionality/features as a independent service (usually referred to a backend application) with well defined remote interefaces (APIs)",
         "actions": [
             {
@@ -59,7 +59,7 @@ var model ={
         ]
     },
     "lets-define-api-spec" : {
-        "image" : "images/3.png",
+        "image" : "images/build_app/3.png",
         "text" : "It's a good practice to start with spec and make sure, it will help deliver the kind of user experience you want to deliver, Good Choice!, whats next?",
         "actions": [
             {
@@ -73,7 +73,7 @@ var model ={
         ]
     },
     "lets-implement-the-spec": {
-        "image": "images/blank.png",
+        "image": "images/build_app/blank.png",
         "text" : "Again, while its tempting to start coding and implement the spec, remember other teams (UX) who might be waiting on you until your backend is ready. In the interest of speed and productivity of the overall teams, its a best practice to setup a sandbox first, before you are gone coding",
         "actions": [
             {
@@ -83,7 +83,7 @@ var model ={
         ]
     },
     "lets-setup-a-sandbox-with-mock":{
-        "image": "images/4.png",
+        "image": "images/build_app/4.png",
         "text" : "Good Choice!, Setting up a sandbox is quick and enables other teams (UX in this case) to start playing with it, before the real backend is ready. More importantly, you might also get feedback on the APIs itself, if its not enough to deliver the intended user experience, What's next?",
         "actions": [
             {
@@ -97,7 +97,7 @@ var model ={
         ]
     },
     "build-webapp-test-user-experience" : {
-        "image" : "images/blank.png",
+        "image" : "images/build_app/blank.png",
         "text" : "Remember the hint?, While it's a good idea to test it out, usually channel experiences are built by dedicated teams",
         "actions" : [
             {
@@ -107,7 +107,7 @@ var model ={
         ]
     },
     "assemble-teams-building-web-mobile-voice-apps" : {
-        "image": "images/5.png",
+        "image": "images/build_app/5.png",
         "text" : "While you might be thinking, whether assembling the UX teams is the first step or whether it fits here, Remember you need to have this sorted atleast at this stage when you have the sandbox ready, if you can do it earlier, its even better and gives them an opportunity to collaborate on the API spec as well, So far very good, What's next?",
         "actions" : [
             {
@@ -121,7 +121,7 @@ var model ={
         ]
     },
     "create-pdf-send-to-ux-teams" : {
-        "image" : "images/blank.png",
+        "image" : "images/build_app/blank.png",
         "text" : "It's good to enable self-service collaboration between teams from the start, having manual processes to share docs or getting access or getting credentials slows down all the teams and extremely inefficient",
         "actions" : [
             {
@@ -131,7 +131,7 @@ var model ={
         ]
     },
     "publish-apis-portal-enable-self-service" : {
-        "image" : "images/6.png",
+        "image" : "images/build_app/6.png",
         "text" : "Good Going, Self-service FTW!!, Portal is the go-to place all your dependent teams (API Consumers), where they can discover new APIs, learn how to use them (you can publish documents based on your OpenAPI spec), get access and start building application using them. Now how to implement the backend",
         "actions" : [
             {
@@ -145,7 +145,7 @@ var model ={
         ]
     },
     "lets-start-building-java": {
-        "image": "images/blank.png",
+        "image": "images/build_app/blank.png",
         "text": "You can chose any programming language of your choice, but make sure you spent time thinking about the application architecture, so that it helps you not only build faster, but also manageable in the long term as your application functionality grows over time",
         "actions" : [
             {
@@ -155,7 +155,7 @@ var model ={
         ]
     },
     "lets-break-down-microservices" : {
-        "image" : "images/7.png",
+        "image" : "images/build_app/7.png",
         "text" : "There are several application architectures, Microservices is one of them and is gaining lot of momemtum among developers and even businesses for the benefits it brings (you sure would have heard about them), if you are starting new, it might be better for you to design your applications in microservices based architecture, Most of the times its helpful",
         "actions" : [
             {
@@ -169,7 +169,7 @@ var model ={
         ]
     },
     "got-the-requirements-lets-go-coding": {
-        "image": "images/blank.png",
+        "image": "images/build_app/blank.png",
         "text": "Before you code, lets also make sure what needs to be codified vs configured, have you identified the business logic vs business policie and how to implement them?",
         "actions" : [
             {
@@ -179,7 +179,7 @@ var model ={
         ]
     },
     "lets-separate-business-logic-business-policy" : {
-        "image" : "images/8.png",
+        "image" : "images/build_app/8.png",
         "text" : "Carefully separate Business logic vs Business Policy, code only your business logic, leverage platform capabilities (have you chosen your platform yet?) to apply business policies",
         "actions" : [
             {
@@ -193,7 +193,7 @@ var model ={
         ]
     },
     "write-new-microservices-for-biz-policies" : {
-        "image": "images/blank.png",
+        "image": "images/build_app/blank.png",
         "text": "If you are chosing the right platform to build applications, you don't need to actually code your business policies, the platform usually provides you the option to configure them",
         "actions" : [
             {
@@ -203,7 +203,7 @@ var model ={
         ]
     },
     "leverge-platoform-configure-biz-policies" : {
-        "image" : "images/9.png",
+        "image" : "images/build_app/9.png",
         "text" : "Modern applications are built on application development platforms that are cloud-native, provides tools for developers to build and manage their applications. They also provide ability for developers to configure policies / rules for their applications ranging from security, traffic management, monitoring and analytics. [in this case, Apigee and Istio are part of such an platform, that helps developers with managing APIs, services and managing business policies for them]",
         "actions" : [
             {
@@ -217,7 +217,7 @@ var model ={
         ]
     },
     "call-web-team-test-new-api" : {
-        "image": "images/blank.png",
+        "image": "images/build_app/blank.png",
         "text": "Remember we had setup a portal earlier, do you want to still call them?",
         "actions" : [
             {
@@ -227,7 +227,7 @@ var model ={
         ]
     },
     "publish-newimplementation-notify-other-teams" : {
-        "image" : "images/10.png",
+        "image" : "images/build_app/10.png",
         "text" : "That's how you do it, Self-service FTW, you publish to the portal, interested parties get notified [your UX teams hopefully are still interested :)], they pick up the new version and start embedding in their apps, Great Job!, Well done!",
         "actions" : [
             {
--- build-an-app/public/js/index.js
+++ build-an-app/public/js/index.js
@@ -1,11 +1,19 @@
+var model;
 $( document ).ready(function() {
-    console.log( "ready!" );
-    M.AutoInit();
-
+    console.log( "ready!" );    
+    var m=getParameterByName('model')
+    setModel(m)
     console.log(model)
-    goto("start");
+    if(model)
+      goto("start");
 });
 
+function setModel(m){
+  if (m=='build_model')
+    model=build_model
+  else
+    model=migrate_model  
+}
 
 function goto(index){
     var s = model[index]
@@ -23,4 +31,14 @@ function goto(index){
         $('#placeholder').html(rendered);
         console.log(rendered)
     });
+}
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
